201c1946c1312b9ba165d0f5810fdce1df731fcb
haidell <haihoing@qq.com>
2026-04-12 23:37:01 +0800

    26041223043d5621d85f36c38a72953d

--- python/test.py
+++ python/test.py
@@ -2,12 +2,15 @@ import wcocr
 import os, json
 
 if os.name == 'nt':
-    wechat_path = r"D:\Program Files\Tencent\Weixin\4.1.7.59"
-    wechatocr_path = os.getenv("APPDATA") + r"\Tencent\xwechat\XPlugin\Plugins\WeChatOcr\8082\extracted\wxocr.dll"
+    wechat_path = r"..\wechatocr\wechat\4.1.7.59"
+    wechatocr_path = r"..\wechatocr\wechatocr\8082\extracted\wxocr.dll"
 else:
     wechat_path = "/opt/wechat"
     wechatocr_path = "/opt/wechat/wxocr"
 
-wcocr.init(wechatocr_path, wechat_path)
+assert os.path.exists(wechat_path), wechat_path
+assert os.path.exists(wechatocr_path), wechatocr_path
+
+wcocr.init(os.path.abspath(wechatocr_path), os.path.abspath(wechat_path))
 result = wcocr.ocr("test.png")
 print(json.dumps(result, ensure_ascii=False, indent=2))
